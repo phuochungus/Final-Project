43a7fa573fddd93fcce1537f3025779295ce141b
phuochungus <21520252@gm.uit.edu.vn>
2022-12-06 13:34:35 +0700

    dev: add new ICommand, model and static methods to handle HomeView interaction

---
+++ Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using _4NH_HAO_Coffee_Shop.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4NH_HAO_Coffee_Shop.Model
+{
+
+    public class Product
+    {
+        public Item Key { get; set; }
+        public int Value { get; set; }
+        public Product(Item item, int value) { Key = item; Value = value; }
+    }
+}
--- ViewModel/HomeViewModel.cs
+++ ViewModel/HomeViewModel.cs
@@ -11,6 +11,9 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
+using static System.Net.WebRequestMethods;
+using System.Windows.Media;
 
 namespace _4NH_HAO_Coffee_Shop.ViewModel
 {
@@ -25,40 +28,72 @@ namespace _4NH_HAO_Coffee_Shop.ViewModel
         public ICommand CategoryChangeCommand { get; set; }
 
         private ObservableCollection<Item> _categorizedItemList;
-        public ObservableCollection<Item> categorizedItemList { get => _categorizedItemList; set { _categorizedItemList= value; OnPropertyChanged(); } }
+        public ObservableCollection<Item> categorizedItemList { get => _categorizedItemList; set { _categorizedItemList = value; OnPropertyChanged(); } }
 
         private int _currentCategory;
-        public int currentCategory { get => _currentCategory; 
-            set { 
+        public int currentCategory
+        {
+            get => _currentCategory;
+            set
+            {
                 if (value != currentCategory)
                 {
                     _currentCategory = value;
                     OnPropertyChanged();
                 }
-            } }
+            }
+        }
 
+        public ICommand AddToBillCommand { get; set; }
+        public ICommand UpdateQuantityCommand { get; set; }
+        public ICommand DecreaseQuantityCommand { get; set; }
+        public ICommand IncreaseQuantityCommand { get; set; }
 
-       
 
         public HomeViewModel()
         {
+
             //
             int ChosenCategoryID = -1;// Determine which CategoryID is chosen to be shown
-            //
+                                      //
 
+            //
+            categorizedItemList = new ObservableCollection<Item>(DataProvider.Ins.DB.Items.Where(Cond => Cond.DisplayName=="Cafe đen").ToList());
+
+            //
+            AddToBillCommand = new RelayCommand<object>((p) => true, (p) => { Globals.Insert(p as Item); });
+            UpdateQuantityCommand = new RelayCommand<object>(p => true, p =>
+            {
+                var values = (object[])p;
+                var item = values[0] as Item;
+                var quantity = (int)values[1];
+                Globals.Update(item, quantity);    
+            });
+            DecreaseQuantityCommand = new RelayCommand<object>((p) => true, p =>
+            {
+                var values = (object[])p;
+                var item = values[0] as Item;
+                var quantity = (int)values[1];
+                Globals.Delete(item);
+            });
+            IncreaseQuantityCommand = new RelayCommand<object>((p) => true, p =>
+            {
+                var values = (object[])p;
+                var item = values[0] as Item;
+                var quantity = (int)values[1];
+                Globals.Insert(item);
+            });
             CategoryList = new ObservableCollection<Category>(DataProvider.Ins.DB.Categories.ToList());
-            
-            CategoryChangeCommand = new RelayCommand<object>((p) => { 
+
+            CategoryChangeCommand = new RelayCommand<object>((p) =>
+            {
                 return true;
-            }, (p) => {
+            }, (p) =>
+            {
                 ChosenCategoryID = (int)p;
                 categorizedItemList = new ObservableCollection<Item>(DataProvider.Ins.DB.Items.Where(Cond => Cond.CategoryId == ChosenCategoryID).ToList());
             });
 
-
-
-
-            
         }
     }
 }
