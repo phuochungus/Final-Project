d0608fb55f47bf593897040f0b931d818981e33e
KIWI\ADMIN <21522217@gm.uit.edu.vn>
2022-12-13 23:08:41 +0700

    update basic controls

--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -1,12 +1,19 @@
-
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Navigation;
+using MaterialDesignThemes.Wpf;
 using _4NH_HAO_Coffee_Shop.Model;
+using System.Net.Mail;
 using System.Security.Cryptography;
+using _4NH_HAO_Coffee_Shop.View;
 using System.Data.Entity;
 
 namespace _4NH_HAO_Coffee_Shop.ViewModel
@@ -57,6 +64,7 @@ namespace _4NH_HAO_Coffee_Shop.ViewModel
 
         public bool inputCheck()
         {
+
             return true;
             /////-----
             //if (!isValidEmail(_email) || _password == null || _password == "") return false;
@@ -75,28 +83,18 @@ namespace _4NH_HAO_Coffee_Shop.ViewModel
             //    }
             //};
         }
-        public string CreateMD5(string password)
-        {
-            byte[] encodedPassword = new UTF8Encoding().GetBytes(password);
-            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
 
-            string encoded = BitConverter.ToString(hash)
-               .Replace("-", string.Empty)
-               .ToLower();
-            return encoded;
-        }
 
 
         public async void handleLoginButtonPress(Window p)
         {
-            Email = "nguyenvana@gmail.com";
-            Password= "password";
-            if (p == null) return;
-            ProgressBar = Visibility.Visible;
-            LoginButton = Visibility.Hidden;
-            
             try
             {
+                Email = "nguyenvana@gmail.com";
+                Password = "password";
+                if (p == null) return;
+                ProgressBar = Visibility.Visible;
+                LoginButton = Visibility.Hidden;
                 string EncryptedPassword = CreateMD5(Password);
                 Globals.Instance.CurrUser = await DataProvider.Ins.DB.Accounts.Where(x => x.Email == Email && x.Password == EncryptedPassword).FirstOrDefaultAsync();
                 ProgressBar = Visibility.Hidden;
@@ -111,11 +109,20 @@ namespace _4NH_HAO_Coffee_Shop.ViewModel
                     p.Hide();
                 }
             }
-            catch (Exception e)
+            catch 
             {
-                Console.WriteLine(e);
-                throw;
+                MessageBox.Show("My Ass is On Fire");
             }
+        }
+
+
+        public string CreateMD5(string password)
+        {
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(password);
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+
+            string encoded = BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
+            return encoded;
         }
     }
 }
