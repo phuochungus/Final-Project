35f7a3b521b34a5e59531c6b684374602a5e5545
KIWI\ADMIN <21522217@gm.uit.edu.vn>
2023-01-04 13:51:30 +0700

    update setting view

--- ViewModel/GeneralControlBarViewModel.cs
+++ ViewModel/GeneralControlBarViewModel.cs
@@ -1,19 +1,31 @@
 ﻿
+using MaterialDesignThemes.Wpf;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace _4NH_HAO_Coffee_Shop.ViewModel
 {
-    public  class GeneralControlBarViewModel : BaseViewModel
+    public class GeneralControlBarViewModel : BaseViewModel
     {
         public ICommand DragMoveWindowCommand { get; set; }
         public ICommand ResizeWindowCommand { get; set; }
-        public GeneralControlBarViewModel() 
+        public ICommand ChangeControlBarColor { get; set; }
+
+
+        private System.Windows.Media.Brush _BGColor;
+        public System.Windows.Media.Brush BGColor { get => _BGColor; set { _BGColor = value; OnPropertyChanged(); } }
+
+        public GeneralControlBarViewModel()
         {
-            DragMoveWindowCommand = new RelayCommand<UserControl>((p) => {
+            DragMoveWindowCommand = new RelayCommand<UserControl>((p) =>
+            {
                 return true;
-            }, (p) => {
+            }, (p) =>
+            {
                 FrameworkElement window = GetWindowParent(p);
                 Window w = window as Window;
                 if (w != null)
@@ -23,9 +35,11 @@ namespace _4NH_HAO_Coffee_Shop.ViewModel
                 }
             });
 
-            ResizeWindowCommand = new RelayCommand<UserControl>((p) => {
+            ResizeWindowCommand = new RelayCommand<UserControl>((p) =>
+            {
                 return true;
-            }, (p) => {
+            }, (p) =>
+            {
                 FrameworkElement window = GetWindowParent(p);
                 Window w = window as Window;
                 if (w != null)
@@ -35,6 +49,16 @@ namespace _4NH_HAO_Coffee_Shop.ViewModel
                     else if (w.WindowState != WindowState.Normal)
                         w.WindowState = WindowState.Normal;
                 }
+            });
+
+            ChangeControlBarColor = new RelayCommand<ColorPicker>((p) =>
+            {
+                return true;
+            }, (p) =>   
+            {
+                BaseViewModel us = new SettingViewModel();
+                
+                MessageBox.Show(p.Color.ToString());
             });
         }
 
--- ViewModel/SettingViewModel.cs
+++ ViewModel/SettingViewModel.cs
@@ -1,10 +1,17 @@
-﻿namespace _4NH_HAO_Coffee_Shop.ViewModel
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace _4NH_HAO_Coffee_Shop.ViewModel
 {
     public class SettingViewModel : BaseViewModel
     {
-        public SettingViewModel() 
-        { 
-        
+
+        public SettingViewModel()
+        {
+
         }
     }
 }
