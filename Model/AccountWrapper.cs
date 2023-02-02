2ed377750de845d06c309ed18e5761a3c4e76fab
unknown <you@example.com>
2023-02-02 18:01:17 +0700

    update AccountWrapper

---
+++ Model/AccountWrapper.cs
@@ -0,0 +1,123 @@
+﻿using _4NH_HAO_Coffee_Shop.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4NH_HAO_Coffee_Shop.Model
+{
+    public class AccountWrapper : BaseViewModel
+    {
+        public Account account;
+        public string Id
+        {
+            get => account.Id;
+            set
+            {
+                account.Id = value;
+                OnPropertyChanged();
+            }
+        }
+        public string DisplayName
+        {
+            get => account.DisplayName;
+            set
+            {
+                account.DisplayName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get => account.Email;
+            set
+            {
+                account.Email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => account.PhoneNumber;
+            set
+            {
+                account.PhoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => account.Password;
+            set
+            {
+                account.Password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string AccountType
+        {
+            get => account.AccountType;
+            set
+            {
+                account.AccountType = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ImageURL
+        {
+            get => account.ImageURL;
+            set
+            {
+                account.ImageURL = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ManagedBy
+        {
+            get => account.ManagedBy;
+            set
+            {
+                account.ManagedBy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public virtual ICollection<Account> Account1 
+        {
+            get => account.Account1; 
+            set
+            {
+                account.Account1 = value;
+                OnPropertyChanged();
+
+            }
+        }
+        public virtual Account Account2
+        {
+            get => account.Account2;
+            set
+            {
+                account.Account2 = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        public virtual ICollection<Bill> Bills 
+        {
+            get => account.Bills;
+            set
+            {
+                account.Bills = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
--- ViewModel/YourProfileViewModel.cs
+++ ViewModel/YourProfileViewModel.cs
@@ -86,30 +86,41 @@ namespace _4NH_HAO_Coffee_Shop.ViewModel
 
         public YourProfileViewModel()
         {
+            // Sử dụng thông tin tài khoản được lưu trong  Globals.Instance.CurrUser để điền vào các TextBlock tương ứng
             _DisplayName = Globals.Instance.CurrUser.DisplayName;
             _Email = Globals.Instance.CurrUser.Email;
             _PhoneNumber = Globals.Instance.CurrUser.PhoneNumber;
             _AccountType = Globals.Instance.CurrUser.AccountType;
             _ImageSource = Globals.Instance.CurrUser.ImageURL;
+
             ShowHRViewCommand = new RelayCommand<object>((p) =>
             {
+                // Kiểm tra thông tin tài khoản có phải admin
                 if (AccountType == "admin") return true;
                 else return false;
             }, p =>
             {
+                // Khởi tạo cửa sổ HR mới và hiển thị
                 HRView hr = new HRView();
                 hr.ShowDialog();
 
             });
+
             AvatarCommand = new RelayCommand<object>((p) => { return true; }, p =>
             {
+                // Mở filedialod và lọc file theo định dạng png và jpeg
                 OpenFileDialog open = new OpenFileDialog();
                 open.Filter = "File ảnh| *.png;*.jpeg";
+
                 if (open.ShowDialog() == DialogResult.OK)
                 {
+                    // Lưu đường dẫn file vào Imagesource để binding vào hình ảnh
                     ImageSource = open.FileName;
+
+                    // Tìm và lưu ImageSource vào Accounts tương ứng
                     var account = DataProvider.Ins.DB.Accounts.Where(x => x.Id == Globals.Instance.CurrUser.Id).SingleOrDefault();
                     account.ImageURL = ImageSource;
+                    DataProvider.Ins.DB.SaveChanges();
                 }
 
             });
